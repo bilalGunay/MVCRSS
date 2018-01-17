5cf25c729309dfaeb0aec1a7280d6fc9c5888ac3
bilalGunay <e180712@metu.edu.tr>
2018-01-17 22:31:49 +0300

    Rss'e bağlanma ve veri çekme

---
+++ RSSKO/RSSKO/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using RSSKO.Models;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace RSSKO.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            WebClient wclient = new WebClient();
+            string RSSData = wclient.DownloadString("https://www.wired.com/feed/rss");
+
+            XDocument xml = XDocument.Parse(RSSData);
+            var RSSFeedData = (from x in xml.Descendants("item")
+                               select new RssHeader
+                               {
+                                   Title = ((string)x.Element("title")),
+                                   Link = ((string)x.Element("link")),
+                                   Description = ((string)x.Element("description")),
+                                   PubDate = ((DateTime)x.Element("pubDate"))
+                               }).Take(5);
+            ViewBag.RssFeed = RSSFeedData;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ RSSKO/RSSKO/Models/RssHeader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RSSKO.Models
+{
+    public class RssHeader
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+        public DateTime PubDate { get; set; }
+    }
+}
